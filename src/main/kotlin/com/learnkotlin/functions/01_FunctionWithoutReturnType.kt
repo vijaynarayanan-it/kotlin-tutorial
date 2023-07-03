e409a5dcd53ab5488c08525638ef3d6654381af4
Vijay Narayanan <vijaynarayanan.it@gmail.com>
2023-07-03 22:10:34 +0200

    Functions with return type and without return type

---
+++ src/main/kotlin/com/learnkotlin/functions/01_FunctionWithoutReturnType.kt
@@ -0,0 +1,28 @@
+package com.learnkotlin.functions
+
+fun main() {
+    printErrorMessage("Ok")
+}
+
+/**
+ * usual function declaration. within parameter,
+ * we should first mention the name of the argument and then its type.
+ *
+ * Since we are not returning anything, this function's default return type is 'Unit'.
+ * 'Unit' is equivalent to void return type in Java.
+ *
+ * ex: *****(status: String, age: Unit)
+ */
+fun printErrorMessage(status: String) {
+    if (status == "Ok") {
+        println("200")
+    } else {
+        println("404")
+    }
+}
+
+/**
+ * Below method is the simplified version of printErrorMessage method.
+ * We can
+ */
+fun printMessage(status: String) = if (status == "Ok") println("200") else println("404")
---
+++ src/main/kotlin/com/learnkotlin/functions/02_FunctionWithReturnType.kt
@@ -0,0 +1,37 @@
+package com.learnkotlin.functions
+
+fun main() {
+    val responseCode = getResponseCode("Ok")
+    println(responseCode)
+}
+
+/**
+ * Below method explains the syntax of a function with return type declaration.
+ *
+ * We need to specify the return type as below
+ *
+ * someMethodName(..) : String {
+ *   .....
+ * }
+ *
+ */
+fun getResponseCode(status: String) : Int {
+    if (status == "Ok") {
+        return 200
+    } else {
+        return 404
+    }
+}
+
+/**
+ * Simplified version 1.
+ *
+ */
+fun getResponse(status: String) : String {
+    return if (status == "Ok") "Created" else "Not created"
+}
+
+/**
+ * Simplified version 2.
+ */
+fun getResponseData(status: String) : String = if (status == "Ok") "Data 1" else "No data"
