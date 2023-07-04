cfc77ee7276c9accd1bd40c455be0b20cfdd39ca
Vijay Narayanan <vijaynarayanan.it@gmail.com>
2023-07-04 11:05:24 +0200

    Usage of @file:JvmName("") annotation

--- src/main/kotlin/com/learnkotlin/functions/Utils.kt
+++ src/main/kotlin/com/learnkotlin/functions/Utils.kt
@@ -1,3 +1,11 @@
+/**
+ * Below annotation helps us to create a custom name for the class name
+ * Without using this annotation we need to call isValidString
+ * method in any Java class using UtilsKt.isValidString(..) method.
+ *
+ * We need to make sure this annotation should always present
+ * in the first line of the class and before the package declaration.
+ */
 @file:JvmName("Validator")
 package com.learnkotlin.functions
 
